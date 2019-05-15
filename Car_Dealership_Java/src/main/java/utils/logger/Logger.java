30981a526ef1ce0c79b4b5dba7250968f07f408e
sebrown0 <thebrownfamily@live.co.uk>
2019-05-15 09:36:35 +0200

    Removed commented code from Logger

--- Car_Dealership_Java/src/main/java/utils/logger/Logger.java
+++ Car_Dealership_Java/src/main/java/utils/logger/Logger.java
@@ -50,24 +50,11 @@ public final class Logger extends FileHandler implements Log {
 		return timer.currentTimeFormatted() + ": " + logData.objectID() + " -> ";
 	}
 	
-	/* 
-	 * Write a log entry to the log file. 
-	 * Assuming once a log is created it is for one day only. 
-	 * So don't write the date for each entry to keep it cleaner.
-	 */
-//	@Override
-//	public void logEntry(String Id, String logEntry) {
-//		LocalDateTime localDt = LocalDateTime.now();
-//		DateTimeFormatter dtFormat = DateTimeFormatter.ofPattern("HH:mm:ss(SSSS)");		
-//		writeFile(logFile, (dtFormat.format(localDt) + " : " + Id + " -> " + logEntry), this);
-//	}
-
 	@Override
 	public void writeNewFile() {
 		LocalDateTime localDt = LocalDateTime.now();
 		DateTimeFormatter dtFormat = DateTimeFormatter.ofPattern("dd/MM/yyyy");
 		this.writeFile(this.logFile, (dtFormat.format(localDt)), this);
-//		logEntry("XXX", "New File Started");
 	}
 	
 	public static class LogHelper {
